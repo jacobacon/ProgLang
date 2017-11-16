09d08927200e3cc8a160740787fedb106564f355
Jacob Beneski <jacobbeneski@gmail.com>
2017-11-15 22:07:19 -0600

    Update source file handling

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from parser import Parser
+from lexer import Lexer
+import sys
+import os.path
 
-p = Parser()
+lexer = Lexer()
 
-sourcefile = open("language.run")
+if len(sys.argv) > 1:
+    if os.path.exists(sys.argv[1]):
+        sourcefile = open(sys.argv[1])
+    else:
+        print("Usage Error: The SourceFile Doesn't Exist.")
+else:
+    print('Usage Error: You Must Provide a Valid Source File to Run.')
+    sys.exit(1)
 
-print p.languagelexer(sourcefile.read())
+
+#print lexer.languagelexer(sourcefile.read())
 
 sourcefile.close()
