10f6824f7415172175e2a45b29ccef7b804bc411
Jacob Beneski <jacobbeneski@gmail.com>
2017-11-26 16:33:53 -0600

    add comments to entry point

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import os.path
 
 lexer = Lexer()
 
+# This is the main entry point for code execution of Cookpy code.
+# This file takes a single argument, which should point to the Cookpy code you want to run.
+# If no argument is given, or if the file doesn't exist an error is raised.
+
 if len(sys.argv) > 1:
     if os.path.exists(sys.argv[1]):
         sourcefile = open(sys.argv[1])
@@ -13,9 +17,15 @@ if len(sys.argv) > 1:
 else:
     raise StandardError('Usage Error: You Must Provide a Valid Source File to Run.')
 
+# The lexer will read the file and return a list of valid words. Comments will be ignored.
+
 tokens = lexer.languagelexer(sourcefile.read())
 sourcefile.close()
 
+# The parser will handle the rest of code execution.
+
 parser.parser(tokens)
+
+# If no error was raised in the parser, we exit with a 'clean' exit code.
 
 sys.exit(0)
